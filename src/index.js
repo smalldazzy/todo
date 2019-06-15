5bd083309896b99aa6bd45d7165cc25ed41c4c00
smalldazzy <oleg.nod@gmail.com>
2019-06-15 21:46:06 +0300

    clear done

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ class ToDo {
         });
         const ulist = document.getElementById('list');
         let lielem = document.createElement('li');
-        lielem.innerHTML = `<input class="check" type="checkbox" style="display: inline-block"><p style="display:inline-block">${task.title}</p><i style="display:inline-block" class="delete"></i>`;
+        lielem.innerHTML = `<input class="check" type="checkbox" style="display: inline-block"><p style="display:inline-block">${task.title}</p><button style="display:inline-block" class="del"> -</button>`;
         console.log(lielem.firstChild);
         if (task.isDone) {lielem.firstChild.checked=true;}
         if(ulist) {ulist.appendChild(lielem);}
@@ -30,7 +30,20 @@ class ToDo {
             // if(ulist) {ulist.appendChild(lielem);}
         
         });
+        }
     }
+    deleteItem(i){
+        this.store.splice(i,1);
+    }
+    deleteDone(){
+        // this.store.forEach((element,index) =>{
+        //     if (element.isDone===true) {this.deleteItem(index);console.log('udoli');}
+        // });
+        for (let i=this.store.length-1;i>=0;i--){
+            if (this.store[i].isDone===true) {this.deleteItem(i);console.log('udoli');}
+        }
+        localStorage.setItem("TODO", JSON.stringify(list.store));
+
     }
     // editItem (){
 
@@ -63,6 +76,13 @@ window.onload=function(){
     list.getItems();
     const submit = document.getElementById('submit');
     const field = document.getElementById('inp');
+    const del = document.getElementsByClassName('del');
+    const deldone = document.getElementById('clear');
+    deldone.addEventListener('click',()=>{
+        list.deleteDone();
+        location.reload();
+    })
+
     submit.addEventListener('click', function (){
         console.log(field.value);
         let task2=new TaskItem(Math.floor(Math.random()*100),field.value);
@@ -70,22 +90,34 @@ window.onload=function(){
         // list.getItems(ulist);
         console.log(list.store);
         localStorage.setItem("TODO", JSON.stringify(list.store));
+        location.reload();//cures initial checkbox fault, clears inputbox
 
     });
     let check = document.getElementsByClassName('check');
     console.log(check);
+
+    //add event bubbling
     for (let i=0;i<check.length;i++){
+        //arrow func
         check[i].addEventListener('change', function(){
             if (this.checked) {
-                console.log('na jebalo najmi');
                 console.log(list.store[i]);
                 list.store[i].isDone=true;
                 localStorage.setItem("TODO", JSON.stringify(list.store));
             } else {
-                console.log('molodets');
                 list.store[i].isDone=false;
                 localStorage.setItem("TODO", JSON.stringify(list.store));
             }
+        })
+    }
+
+    for (let i=0;i<del.length;i++){
+        //arrow func
+        del[i].addEventListener('click', function (){
+            list.deleteItem(i);
+            localStorage.setItem("TODO", JSON.stringify(list.store));
+            location.reload(); //bad way
+
         })
     }
     const ulist = document.getElementById('list');
